10f4ac10364a3e86ae8477bccbe454c656943189
Yann Ray <51749201+artifroid42@users.noreply.github.com>
2023-06-20 15:05:26 +0200

    AK Sound

---
+++ EggacyUnityProject/Assets/Eggacy/Sound/SoundPoolManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eggacy.Sound
+{
+    public class SoundPoolManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource m_audioSource;
+
+        public List<SoundEventData> RegisteredSoundEventData;
+
+        private void Awake()
+        {
+            foreach (var soundEventData in RegisteredSoundEventData)
+            {
+                soundEventData.on2DPlayRequested += Play2DSound;
+                soundEventData.on3DPlayRequested += Play3DSound;
+            }
+        }
+
+        private void Play3DSound(SoundEventData soundEventData, Transform transform)
+        {
+            var audioSource = Instantiate(m_audioSource, transform.position, Quaternion.identity, this.transform);
+            audioSource.clip = soundEventData.AudioClip;
+            audioSource.Play();
+        }
+
+        private void Play2DSound(SoundEventData soundEventData)
+        {
+            var audioSource = Instantiate(m_audioSource, this.transform);
+            audioSource.clip = soundEventData.AudioClip;
+            audioSource.Play();
+        }
+    }
+}
+
