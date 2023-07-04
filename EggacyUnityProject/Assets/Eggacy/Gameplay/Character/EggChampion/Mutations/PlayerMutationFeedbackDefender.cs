eab2fb72ae95925ba1c7bc6e72145b4c1989df59
Yann Ray <51749201+artifroid42@users.noreply.github.com>
2023-07-04 15:40:39 +0200

    Mutations FXs feedbacks

---
+++ EggacyUnityProject/Assets/Eggacy/Gameplay/Character/EggChampion/Mutations/PlayerMutationFeedbackDefender.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eggacy.Gameplay.Character.EggChampion.Mutations
+{
+    public class PlayerMutationFeedbackDefender : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> m_feedbacks;
+
+        [SerializeField]
+        private DefenderMutation m_mutation;
+
+        private void Start()
+        {
+            m_mutation.onLevelUpdated += OnLevelUpdated;
+        }
+
+        private void Awake()
+        {
+            foreach (var feedback in m_feedbacks)
+            {
+                feedback.SetActive(false);
+            }
+        }
+
+        private void OnLevelUpdated(AMutation<DefenderMutationLevelData> obj)
+        {
+            foreach (var feedback in m_feedbacks)
+            {
+                feedback.SetActive(true);
+            }
+        }
+    }
+}
+
