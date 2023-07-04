d4affbb28346110582ee2ac219b9bd28249d9224
Fergal MECHIN <mechin.fergal@gmail.com>
2023-07-04 14:26:03 +0200

    Rewrk heal

--- EggacyUnityProject/Assets/Eggacy/Gameplay/Character/ChickenTank/SelfHealing/ChickenTankSelfHealingController.cs
+++ EggacyUnityProject/Assets/Eggacy/Gameplay/Character/ChickenTank/SelfHealing/ChickenTankSelfHealingController.cs
@@ -1,5 +1,6 @@
 using Eggacy.Gameplay.Combat.LifeManagement;
 using Fusion;
+using System;
 using UnityEngine;
 
 namespace Eggacy.Gameplay.Character.ChickenTank.SelfHealing
@@ -16,16 +17,44 @@ namespace Eggacy.Gameplay.Character.ChickenTank.SelfHealing
 
         private float _lastTimeOfHealing = float.MinValue;
 
+        [SerializeField]
+        private int _damageReceivedToGoInBattle = 40;
+        [SerializeField]
+        private int _damageReceivedDescreasingSpeed = 20;
+        private float _damageReceived = 0;
+
+        private void Start()
+        {
+            _lifeController.onDamageDealt_ServerOnly += HandleDamageDealt;
+        }
+
+        private void HandleDamageDealt(LifeController arg1, int arg2, LifeController arg3)
+        {
+            _damageReceived += arg2;
+        }
+
+        private void OnDestroy()
+        {
+            if(_lifeController)
+                _lifeController.onDamageDealt_ServerOnly += HandleDamageDealt;
+        }
+
         private void Update()
         {
             if (!Runner) return;
             if(!Runner.IsServer) return;
 
-            if(Time.time - _lastTimeOfHealing > _healingCooldown)
+            _damageReceived -= _damageReceivedDescreasingSpeed * Time.deltaTime;
+            if(_damageReceived < 0)
             {
-                _lifeController.Heal(_healingAmount);
-                _lastTimeOfHealing = Time.time;
-            }
+                _damageReceived = 0;
+
+                if (Time.time - _lastTimeOfHealing > _healingCooldown)
+                {
+                    _lifeController.Heal(_healingAmount);
+                    _lastTimeOfHealing = Time.time;
+                }
+            }   
         }
     }
 }
